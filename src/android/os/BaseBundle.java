2c9ef1a60fc3c6dd97a998c5bcb179457c2394c2
juxtenia <juxtenia@googlemail.com>
2016-01-20 17:45:32 +0000

    Update BaseBundle.java

--- src/android/os/BaseBundle.java
+++ src/android/os/BaseBundle.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2014 The Android Open Source Project
  *
- * Modified by James Riordan in Jan 2016, to remove references to  other android classes
+ * Modified by James Riordan in Jan 2016, to remove references to other android classes
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
