bb3cf1d3e3143efbc01ce03053490a97a4197908
Tim Ringland <ringlandtim@gmail.com>
2016-01-28 17:59:29 +0000

    Refactored my package from echo2016 to echo2016.multinarrative

--- src/main/java/uk/ac/cam/echo2016/multinarrative/MultiNarrative.java
+++ src/main/java/uk/ac/cam/echo2016/multinarrative/MultiNarrative.java
@@ -1,4 +1,14 @@
 package uk.ac.cam.echo2016.multinarrative;
 
-class MultiNarrative{
+/**
+ * 
+ * @author tr393
+ *
+ */
+public class MultiNarrative { //TODO Todo's and documentation
+	
+	public Node getNode(String id) {return null;}
+	
+	public Narrative getNarrative(String id) {return null;}
+	
 }
--- src/main/java/uk/ac/cam/echo2016/multinarrative/NarrativeTemplate.java
+++ src/main/java/uk/ac/cam/echo2016/multinarrative/NarrativeTemplate.java
@@ -1,4 +1,4 @@
-package uk.ac.cam.echo2016;
+package uk.ac.cam.echo2016.multinarrative;
 
 /**
  * 
