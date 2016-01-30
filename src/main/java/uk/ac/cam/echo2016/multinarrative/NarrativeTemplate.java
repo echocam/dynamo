d003d6a29a92b68db60d1a7a21dc17528f4a1df0
Tim Ringland <ringlandtim@gmail.com>
2016-01-30 15:43:40 +0000

    Fixed a load of TODO's and added some javadoc. Also extended Test Class.

--- src/main/java/uk/ac/cam/echo2016/multinarrative/NarrativeTemplate.java
+++ src/main/java/uk/ac/cam/echo2016/multinarrative/NarrativeTemplate.java
@@ -5,13 +5,12 @@ package uk.ac.cam.echo2016.multinarrative;
  * @author tr393
  *
  */
-public class NarrativeTemplate extends MultiNarrative { // TODO Todo's and
-														// documentation
+public class NarrativeTemplate extends MultiNarrative { // TODO Todo's and documentation
 	public NarrativeInstance generateInstance() {
 		NarrativeInstance instance = new NarrativeInstance();
 		
 		instance.start = this.start.copy(instance);
-		for (Node node : this.nodes) { // TODO might be breaking encapsulation
+		for (Node node : this.nodes) {
 			node.setCopied(false); // TODO bad encapsulation, but resets copy flag
 		}
 		return instance;
