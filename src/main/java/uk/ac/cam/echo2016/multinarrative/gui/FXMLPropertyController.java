36e4a3e8ac3d34c53938706e75d40f155db2d3d0
James <jr650@cam.ac.uk>
2016-02-04 10:03:57 +0000

    Added value addition and removal

--- src/main/java/uk/ac/cam/echo2016/multinarrative/gui/FXMLPropertyController.java
+++ src/main/java/uk/ac/cam/echo2016/multinarrative/gui/FXMLPropertyController.java
@@ -2,12 +2,14 @@ package uk.ac.cam.echo2016.multinarrative.gui;
  
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
+import javafx.scene.control.cell.TextFieldListCell;
 import javafx.scene.layout.BorderPane;
  
 /**
@@ -34,6 +36,8 @@ public class FXMLPropertyController {
         this.name.setText(name);
         propName = name;
         controller=parent;
+        
+        values.setCellFactory(TextFieldListCell.forListView());
         
         this.name.textProperty().addListener(new ChangeListener<String>() {
             @Override
@@ -63,5 +67,26 @@ public class FXMLPropertyController {
     protected void deleteButtonAction(ActionEvent event){
     	controller.removeProperty(propName, root);
     }
-
+    
+    @FXML
+    protected void addValueAction(ActionEvent event){
+    	ObservableList<String> items = values.getItems();
+    	int i = 1;
+    	String s = Strings.populateString(Strings.PROPERTY_VALUE, ""+i);
+    	while(items.contains(s)){
+    		i++;
+    		s=Strings.populateString(Strings.PROPERTY_VALUE, ""+i);
+    	}
+    	items.add(s);
+    }
+    
+    @FXML
+    protected void removeValueAction(ActionEvent event){
+    	ObservableList<String> selected = values.getSelectionModel().getSelectedItems();
+    	ObservableList<String> items = values.getItems();
+    	for(String s: selected){
+    		items.remove(s);
+    	}
+    }
+ 
 }
