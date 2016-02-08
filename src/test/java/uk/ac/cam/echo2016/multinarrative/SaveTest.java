fcd1ad29f7e0d6f6d8b2ba7597a45de7ec7bc037
Tim Ringland <ringlandtim@gmail.com>
2016-02-08 22:44:57 +0000

    actually committed updates to angus' code

--- src/test/java/uk/ac/cam/echo2016/multinarrative/SaveTest.java
+++ src/test/java/uk/ac/cam/echo2016/multinarrative/SaveTest.java
@@ -24,10 +24,15 @@ public class SaveTest {
         SynchronizationNode s3 = new SynchronizationNode("right");
         SynchronizationNode s4 = new SynchronizationNode("end");
         Route l1 = new Route("link1", "Chris", s1, c1);
+        l1.setup();
         Route l2 = new Route("link2", "Chris", c1, s2);
+        l2.setup();
         Route l3 = new Route("link3", "Chris", c1, s3);
+        l3.setup();
         Route l4 = new Route("link4", "Chris", s2, s4);
+        l4.setup();
         Route l5 = new Route("link5", "Chris", s3, s4);
+        l5.setup();
         gNarr.addNode(s1);
         gNarr.addNode(c1);
         gNarr.addNode(s2);
@@ -48,20 +53,20 @@ public class SaveTest {
     @Test
     public void structureTest() {
         assertEquals("Check root is correct", "start", gNarr.start.getId());
-        assertEquals("Check root has correct exit route", "link1", gNarr.start.getOptions().get(0).getId());
-        Node c1 = gNarr.start.getOptions().get(0).getEnd();
+        assertEquals("Check root has correct exit route", "link1", gNarr.start.getExiting().get(0).getId());
+        Node c1 = gNarr.start.getExiting().get(0).getEnd();
         assertEquals("Check child of link1", "choice1", c1.getId());
-        assertEquals("Check choice1 has correct number of children", 2, c1.getOptions().size());
-        assertEquals("Check choice1 has correct left route", "link2", c1.getOptions().get(0).getId());
-        assertEquals("Check choice1 has correct right route", "link3", c1.getOptions().get(1).getId());
-        Node left = c1.getOptions().get(0).getEnd();
-        Node right = c1.getOptions().get(1).getEnd();
+        assertEquals("Check choice1 has correct number of children", 2, c1.getExiting().size());
+        assertEquals("Check choice1 has correct left route", "link2", c1.getExiting().get(0).getId());
+        assertEquals("Check choice1 has correct right route", "link3", c1.getExiting().get(1).getId());
+        Node left = c1.getExiting().get(0).getEnd();
+        Node right = c1.getExiting().get(1).getEnd();
         assertEquals("Check choice1 has correct left child", "left", left.getId());
         assertEquals("Check choice1 has correct right child", "right", right.getId());
-        assertEquals("Check left has correct child route", "link4", left.getOptions().get(0).getId());
-        assertEquals("Check right has correct child route", "link5", right.getOptions().get(0).getId());
-        assertEquals("Check left leads into end node", "end", left.getOptions().get(0).getEnd().getId());
-        assertEquals("Check right leads into end node", "end", right.getOptions().get(0).getEnd().getId());
+        assertEquals("Check left has correct child route", "link4", left.getExiting().get(0).getId());
+        assertEquals("Check right has correct child route", "link5", right.getExiting().get(0).getId());
+        assertEquals("Check left leads into end node", "end", left.getExiting().get(0).getEnd().getId());
+        assertEquals("Check right leads into end node", "end", right.getExiting().get(0).getEnd().getId());
 
     }
 }
