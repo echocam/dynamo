7c6c1a35bb6be50a8847e8f02c3818f6360a5353
Elise <eyxue@mit.edu>
2016-02-08 02:01:35 +0000

    filled out function stubs in GUIOperations. Added cycle detection to graph

--- src/main/java/uk/ac/cam/echo2016/multinarrative/gui/GUIOperations.java
+++ src/main/java/uk/ac/cam/echo2016/multinarrative/gui/GUIOperations.java
@@ -1,19 +1,44 @@
 package uk.ac.cam.echo2016.multinarrative.gui;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Queue;
 
+import uk.ac.cam.echo2016.multinarrative.GUINarrative;
+import uk.ac.cam.echo2016.multinarrative.GraphElementNotFoundException;
+import uk.ac.cam.echo2016.multinarrative.Node;
+import uk.ac.cam.echo2016.multinarrative.NonUniqueIdException;
+import uk.ac.cam.echo2016.multinarrative.Route;
 import android.os.BaseBundle;
 import static uk.ac.cam.echo2016.multinarrative.gui.Strings.*;
 
 /**
- * @author jr650, eyx20
+ * @author jr650
+ * @author eyx20
  */
 public class GUIOperations{
+    
+    private GUINarrative multinarrative;
 
     private HashMap<String, BaseBundle> properties;
+    private HashMap<String, Node> nodeslist;
+    private HashMap<String, Coordinate> nodes;
+    private static final String NODE_PREFIX = "Node-";
+    private static int nodeCounter = 1;
+    private static final String NARRATIVE_PREFIX = "Narrative-";
+    private static int narrativeCounter = 1;
     
     /**
-     * TODO
+     * Construtor. Creates a new GUIOperations.
+     */
+    public GUIOperations() {
+        properties = new HashMap<String, BaseBundle>();
+        nodes = new HashMap<String, Coordinate>();
+        nodeslist = new HashMap<String, Node>();
+    }
+    
+    /**
      * Adds the required property
      * @throws IllegalOperationException if can't add property, 
      * message of exception is displayed to the user, using the Strings
@@ -75,7 +100,6 @@ public class GUIOperations{
     }
     
     /**
-     * TODO
      * Removes the required property
      * @throws IllegalOperationException if can't remove property, 
      * message of exception is displayed to the user, using the Strings
@@ -89,7 +113,6 @@ public class GUIOperations{
     }
     
     /**
-     * TODO
      * Changes the name of the required property
      * @throws IllegalOperationException if can't rename property, 
      * message of exception is displayed to the user, using the Strings
@@ -111,41 +134,155 @@ public class GUIOperations{
     }
     
     /**
-     * TODO
-     * Gets a name that's not already in the graph...
-     * @return
+     * Gets a name that's not already in the graph.
+     * @return new node name
      */
     public String getUniqueNodeName(){
-	return "Node";
+        String newName = NODE_PREFIX + nodeCounter;
+        if (!nodes.containsKey(newName)) {
+            nodeCounter += 1;
+            return newName;
+        } else {
+            while (nodes.containsKey(newName)) {
+                nodeCounter += 1;
+                newName = NODE_PREFIX + nodeCounter;
+            }
+            nodeCounter += 1;
+            return newName;
+        } 
     }
     
     /**
-     * TODO
      * Adds a node, throwing exception if it fails.
+     * @throws NonUniqueIdException 
      */
-    public void addSynchNode(String name, double x, double y) throws IllegalOperationException{}
+    //TODO: Check if node is out of bounds/illegal coordinate
+    public void addSynchNode(String name, double x, double y) throws IllegalOperationException, NonUniqueIdException{
+        Coordinate coor = new Coordinate(x, y);
+        if (nodes.containsKey(name)) {
+            throw new NonUniqueIdException("Node name already exists.");
+        }
+        for (String nodename : nodes.keySet()) {
+            if (coor.equals(nodes.get(nodename))) {
+                throw new IllegalOperationException("Node already exists at given position.");
+            }
+        }
+        nodes.put(name, coor);
+        multinarrative.newSynchronizationNode(name);
+    }
     
     /**
-     * TODO
      * Repositions a node by the given offset
      */
-    public void translateNode(String name, double x, double y) {}
+  //TODO: Check if node is out of bounds/illegal coordinate
+    public void translateNode(String name, double x, double y) throws IllegalOperationException{
+        if (!nodes.containsKey(name)) {
+            throw new IllegalOperationException("Node does not exist.");
+        }
+        Coordinate coord = nodes.get(name);
+        double transx = coord.getX() + x;
+        double transy = coord.getY() + y;
+        Coordinate newcoord = new Coordinate(transx, transy);
+        for (String nodename : nodes.keySet()) {
+            if (newcoord.equals(nodes.get(nodename))) {
+                throw new IllegalOperationException("Node already exists at given position.");
+            }
+        }
+        nodes.put(name, newcoord);
+        
+    }
 
     /**
-     * TODO
-     * Gets a name that's not already in the graph...
-     * @return
+     * Gets a name that's not already in the graph.
+     * @return unique narrative name
      */
     public String getUniqueNarrativeName(){
-	return "Narrative";
+        String newName = NARRATIVE_PREFIX + narrativeCounter;
+        if (!nodes.containsKey(newName)) {
+            narrativeCounter += 1;
+            return newName;
+        } else {
+            while (nodes.containsKey(newName)) {
+                narrativeCounter += 1;
+                newName = NARRATIVE_PREFIX + narrativeCounter;
+            }
+            narrativeCounter += 1;
+            return newName;
+        } 
     }
     
     /**
-     * TODO
      * Adds a narrative, throwing exception if it fails.
+     * @param name - unique id of the narrative
+     * @param start - starting node. If node does not exist creates a new node.
+     * @param end - ending node. If node does not exist creates a new node.
      * Do cycle detection here!!
+     * @throws GraphElementNotFoundException 
+     * @throws NonUniqueIdException 
      */
-    public void addNarrative(String name, String start, String end)throws IllegalOperationException{
-	
+    public void addNarrative(String name, String start, String end)
+            throws IllegalOperationException, NonUniqueIdException, 
+            GraphElementNotFoundException{       
+        multinarrative.newRoute(name, start, end);
+        DFSCycleDetect cycleDetect = new DFSCycleDetect(multinarrative.getNode(start));
+        if (cycleDetect.hasCycle()) {
+            throw new IllegalOperationException("Cannot add route: Graph will contain a cycle");
+        }
+    }
+    
+    
+    private class Coordinate {
+        private double x;
+        private double y;
+        
+        public Coordinate(double x1, double y1) {
+            x = x1;
+            y = y1;
+        }
+        
+        public double getX() {
+            return x;
+        }
+        
+        public double getY() {
+            return y;
+        }
+    }
+    
+    private class DFSCycleDetect {
+
+        private HashSet<Node> marked;
+        private ArrayList<Node> onStack;
+        private Node node;
+        private boolean hasCycle;
+
+        public DFSCycleDetect(Node node) {
+            this.node = node;
+            marked = new HashSet<Node>();
+            onStack = new ArrayList<Node>();
+            findCycle(node);
+        }
+
+        public boolean hasCycle() {
+            return hasCycle;
+        }
+
+        public void findCycle(Node n) {
+
+            marked.add(n);
+            onStack.add(n);
+
+            for (Route rt : n.getOptions()) {
+                Node next = rt.getEnd();
+                if(!marked.contains(next)) {
+                    findCycle(next);
+                } else if (onStack.contains(next)) {
+                    hasCycle = true;
+                    return;
+                }
+            }
+
+            onStack.remove(n);
+        }
     }
 }
