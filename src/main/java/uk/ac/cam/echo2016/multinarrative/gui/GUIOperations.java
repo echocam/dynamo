19ac66af1e84d2450370a9c1c5b9207ec0e7a8a3
Angus Hammond <angusgh@gmail.com>
2016-02-09 15:46:11 +0000

    Extract Coordinate class into separate file

--- src/main/java/uk/ac/cam/echo2016/multinarrative/gui/GUIOperations.java
+++ src/main/java/uk/ac/cam/echo2016/multinarrative/gui/GUIOperations.java
@@ -256,24 +256,6 @@ public class GUIOperations {
         }
     }
 
-    private class Coordinate {
-        private double x;
-        private double y;
-
-        public Coordinate(double x1, double y1) {
-            x = x1;
-            y = y1;
-        }
-
-        public double getX() {
-            return x;
-        }
-
-        public double getY() {
-            return y;
-        }
-    }
-
     private class DFSCycleDetect {
 
         private HashSet<Node> marked;
