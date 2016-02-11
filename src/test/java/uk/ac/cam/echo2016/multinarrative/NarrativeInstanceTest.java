b7f3d46408fbb636279006b58ab912006769b309
James <jr650@cam.ac.uk>
2016-02-11 16:31:59 +0000

    Removed unnecessary imports

--- src/test/java/uk/ac/cam/echo2016/multinarrative/NarrativeInstanceTest.java
+++ src/test/java/uk/ac/cam/echo2016/multinarrative/NarrativeInstanceTest.java
@@ -1,15 +1,17 @@
 package uk.ac.cam.echo2016.multinarrative;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import android.os.BaseBundle;
-
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 
 public class NarrativeInstanceTest {
     NarrativeTemplate sampleTemplate;
