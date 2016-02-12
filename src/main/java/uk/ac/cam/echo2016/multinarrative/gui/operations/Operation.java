0af33015fcef7b35ac7f1019486e86a57cc6836e
Tom Maenan Read-Cutting <readcuttingt@gmail.com>
2016-02-12 21:46:21 +0000

    Updated Command documentation.

--- src/main/java/uk/ac/cam/echo2016/multinarrative/gui/operations/Operation.java
+++ src/main/java/uk/ac/cam/echo2016/multinarrative/gui/operations/Operation.java
@@ -8,6 +8,10 @@ import java.util.Deque;
  * The Operation class is used to implement most functions in GUIOperations. The reason for this is that anything that
  * affects the graph, must be 'undoable', and this provides a clean interface for enabling that functionality.
  * 
+ * It is an implementation of the "Command" design pattern.
+ * 
+ * @see <a href="https://en.wikipedia.org/wiki/Command_pattern">Wikipedia: Command pattern</a>
+ * 
  * @author tr395
  *
  */
