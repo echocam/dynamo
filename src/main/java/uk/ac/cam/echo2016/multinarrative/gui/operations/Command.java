3c0de51b7e62ab6784b625d75e4f91e6f8fc0732
Tom Maenan Read-Cutting <readcuttingt@gmail.com>
2016-02-12 21:27:07 +0000

    Added code for actually implementing the undo operation.

--- src/main/java/uk/ac/cam/echo2016/multinarrative/gui/operations/Command.java
+++ src/main/java/uk/ac/cam/echo2016/multinarrative/gui/operations/Command.java
@@ -1,14 +1,60 @@
 package uk.ac.cam.echo2016.multinarrative.gui.operations;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
+/**
+ * 
+ * The Command class is used to implement most functions in GUIOperations. The reason for this is that anything that
+ * affects the graph, must be 'undoable', and this provides a clean interface for enabling that functionality.
+ * 
+ * @author tr395
+ *
+ */
 public interface Command {
-    public static final List<Command> history = new ArrayList<Command>();
+    public static final Deque<Command> undoHistory = new ArrayDeque<Command>(64);
+    public static final Deque<Command> redoHistory = new ArrayDeque<Command>(16);
 
+    /**
+     * Executes a command and stores it on the undoHistory Stack.
+     * 
+     * If an exception is thrown by the command, it won't be added to the history.
+     * 
+     * Furthermore the redoHistory is flushed.
+     * 
+     * @param c
+     *            Command that you wish to execute
+     * @throws IllegalOperationException
+     */
     public static void storeAndExecute(Command c) throws IllegalOperationException {
         c.execute();
-        history.add(c);
+        undoHistory.addFirst(c);
+        redoHistory.clear();
+    }
+
+    /**
+     * Undo the last command.
+     */
+    public static void undoLastCommand() {
+        Command c = undoHistory.removeFirst();
+        c.undo();
+        redoHistory.addFirst(c);
+    }
+
+    /**
+     * Redo the last undo. The exception should never really be thrown as only successful commands should be able to
+     * reach this point.
+     * 
+     * @throws IllegalOperationException
+     */
+    public static void redoLastUndo() {
+        Command c = redoHistory.removeFirst();
+        try {
+            c.execute();
+        } catch (IllegalOperationException e) {
+            throw new RuntimeException(e);
+        }
+        undoHistory.addFirst(c);
     }
 
     public void execute() throws IllegalOperationException;
