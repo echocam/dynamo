a14b1b20c0a6be191a214f324ada5380bf6d8033
James <jr650@cam.ac.uk>
2016-02-24 10:56:46 +0000

    Added some more debug statements

--- src/main/java/uk/ac/cam/echo2016/multinarrative/preview/TextPreview.java
+++ src/main/java/uk/ac/cam/echo2016/multinarrative/preview/TextPreview.java
@@ -41,6 +41,7 @@ public class TextPreview {
 
     public void preview(PrintStream output, InputStream input) {
         ArrayList<Route> items = inst.getPlayableRoutes();
+        outputFile(inst.getStart().getId(), output, input, "Triggered");
         while (!items.isEmpty()) {
             GameChoice choice;
             String s = choose(items, output, input);
@@ -49,7 +50,7 @@ public class TextPreview {
 
                 if (choice.hasEvent()) {
                     for(Node n: choice.getEvents()){
-                        outputFile(n.getId(), output, input, "Completed");
+                        outputFile(n.getId(), output, input, "Triggered");
                     }
                     choice.completeEvents();
                 }
